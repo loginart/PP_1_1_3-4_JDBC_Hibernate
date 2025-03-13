85d5956fde7ceb332110b983a3d0823d1b084492
Artem Loginov <Loginarttt@mail.ru>
2025-03-13 22:28:33 +0300

    ok

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,99 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class UserDaoJDBCImpl implements UserDao {
+public class UserDaoJDBCImpl extends Util implements UserDao {
+    Connection connection = getConnection();
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
 
+        String sql = "CREATE TABLE UsersTable (id INTEGER, name VARCHAR(255) NOT NULL , lastName VARCHAR(255) NOT NULL, age INTEGER NOT NULL)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+           preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void dropUsersTable() {
 
-    }
+     String sql = "DROP TABLE UsersTable";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        }
+
+
 
     public void saveUser(String name, String lastName, byte age) {
 
+    String sql = "INSERT INTO UsersTable (name, lastName, age) VALUES (?, ?, ?)";
+    try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        preparedStatement.setString(1, name);
+        preparedStatement.setString(2, lastName);
+        preparedStatement.setByte(3, age);
+        preparedStatement.executeUpdate();
+    }catch (SQLException e){
+        e.printStackTrace();
     }
+    }
+
 
     public void removeUserById(long id) {
 
+    String sql = "DELETE FROM UsersTable WHERE id = ?";
+    try (PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+
+        preparedStatement.setLong(1, id);
+        preparedStatement.executeUpdate();
+    }catch (SQLException e){
+        e.printStackTrace();
+    }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<User>();
+
+        String sql = "SELECT * FROM UsersTable";
+        try(Statement statement= connection.createStatement();) {
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                users.add(user);
+
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return users;
     }
 
     public void cleanUsersTable() {
 
+    String sql = "DELETE FROM UsersTable";
+    try (PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+
+        preparedStatement.executeUpdate();
+    }catch (SQLException e){
+        e.printStackTrace();
+    }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
+import com.mysql.cj.jdbc.Driver;
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.net.ConnectException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String DB_URL = "jdbc:MySQL://localhost:3306/my_db";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "F010994ff010994F";
+
+
+    public  Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        } catch ( SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
